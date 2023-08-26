181f98005e113a3f138e44988b880916df8a430b
sugijit <b.suldsaikhan@gmail.com>
2023-08-26 16:47:37 +0900

    65 going

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
 //画面コントローラー
-var uiController = (function () {})();
+var uiController = (function () {
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    addBtn: ".add__btn",
+  };
+
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+
+    getDomstrings: function () {
+      return DOMstrings;
+    },
+  };
+})();
 
 //家計簿関係のコントローラー
 var financeController = (function () {})();
 
 //中間コントローラー
 var appController = (function (uiController, financeController) {
+  var DOM = uiController.getDomstrings();
   var ctrlAddItem = function () {
-    console.log("Delgetsees ugugdul avlaa");
+    console.log(uiController.getInput());
   };
 
-  document.querySelector(".add__btn").addEventListener("click", function () {
+  document.querySelector(DOM.addBtn).addEventListener("click", function () {
     ctrlAddItem();
   });
 
