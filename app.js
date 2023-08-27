2aaea6fb2e4eca8dce93147c3e4b62f8b0d96dfa
sugijit <b.suldsaikhan@gmail.com>
2023-08-27 22:35:57 +0900

    Lesson 67 入力データを保存できた

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ var uiController = (function () {
   return {
     getInput: function () {
       return {
-        type: document.querySelector(DOMstrings.inputType).value,
-        description: document.querySelector(DOMstrings.inputDescription).value,
-        value: document.querySelector(DOMstrings.inputValue).value,
+        type: document.querySelector(DOMstrings.inputType).value, //inc, exp
+        description: document.querySelector(DOMstrings.inputDescription).value, //
+        value: document.querySelector(DOMstrings.inputValue).value, //
       };
     },
 
@@ -24,20 +24,21 @@ var uiController = (function () {
 
 //家計簿関係のコントローラー
 var financeController = (function () {
+  //private function
   var Income = function (id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
   };
-
+  //private function
   var Expense = function (id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
   };
-
+  //private data
   var data = {
-    allItems: {
+    items: {
       inc: [],
       exp: [],
     },
@@ -46,12 +47,35 @@ var financeController = (function () {
       exp: 0,
     },
   };
+
+  return {
+    addItem: function (type, desc, val) {
+      var item, id;
+
+      if (data.items[type].length === 0) id = 1;
+      else {
+        id = data.items[type][data.items[type].length - 1].id + 1;
+      }
+      if (type === "inc") {
+        item = new Income(id, desc, val);
+      } else {
+        item = new Expense(id, desc, val);
+      }
+      data.items[type].push(item);
+    },
+
+    seeData: function () {
+      return data;
+    },
+  };
 })();
 
 //中間コントローラー
 var appController = (function (uiController, financeController) {
   var ctrlAddItem = function () {
-    console.log(uiController.getInput());
+    var input = uiController.getInput();
+    console.log(input);
+    financeController.addItem(input.type, input.description, input.value);
   };
 
   var setupEventListeners = function () {
