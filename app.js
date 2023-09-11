b0331e0278c2e10e9fe5d292a3aaa587c605af7e
sugijit <b.suldsaikhan@gmail.com>
2023-09-11 21:24:38 +0900

    Lesson 71 financeController calculate doneee!

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var uiController = (function () {
       return {
         type: document.querySelector(DOMstrings.inputType).value, //inc, exp
         description: document.querySelector(DOMstrings.inputDescription).value, //
-        value: document.querySelector(DOMstrings.inputValue).value, //
+        value: parseInt(document.querySelector(DOMstrings.inputValue).value), //
       };
     },
 
@@ -79,6 +79,15 @@ var financeController = (function () {
     this.description = description;
     this.value = value;
   };
+
+  var calculateTotal = function (type) {
+    var sum = 0;
+    data.items[type].forEach(function (el) {
+      sum = sum + el.value;
+    });
+
+    data.totals[type] = sum;
+  };
   //private data
   var data = {
     items: {
@@ -89,9 +98,30 @@ var financeController = (function () {
       inc: 0,
       exp: 0,
     },
+
+    tusuv: 0,
+
+    huvi: 0,
   };
 
   return {
+    tusuvTootsooloh: function () {
+      calculateTotal("inc");
+      calculateTotal("exp");
+
+      data.tusuv = data.totals.inc - data.totals.exp;
+      data.huvi = Math.round(data.totals.inc / (data.totals.exp * 100));
+    },
+
+    tusuviigAvah: function () {
+      return {
+        tusuv: data.tusuv,
+        huvi: data.huvi,
+        totalInc: data.totals.inc,
+        totalExp: data.totals.exp,
+      };
+    },
+
     addItem: function (type, desc, val) {
       var item, id;
 
@@ -119,15 +149,20 @@ var financeController = (function () {
 var appController = (function (uiController, financeController) {
   var ctrlAddItem = function () {
     var input = uiController.getInput();
+    if (input.description !== "" && input.value !== "") {
+      var item = financeController.addItem(
+        input.type,
+        input.description,
+        input.value
+      );
 
-    var item = financeController.addItem(
-      input.type,
-      input.description,
-      input.value
-    );
+      uiController.addListItem(item, input.type);
+      uiController.clearFields();
 
-    uiController.addListItem(item, input.type);
-    uiController.clearFields();
+      financeController.tusuvTootsooloh();
+      var tusuv = financeController.tusuviigAvah();
+      console.log(tusuv);
+    }
   };
 
   var setupEventListeners = function () {
